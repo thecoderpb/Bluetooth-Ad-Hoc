26204cc850c1bad5d8be671b8c1c3c48ca69c4c3
Pratik Byathnal <pratikbyathnal@gmail.com>
2020-03-18 20:35:22 +0530

    Add enable Bluetooth functionality.

--- app/src/main/java/com/pratik/bluetoothadhoc/MainActivity.java
+++ app/src/main/java/com/pratik/bluetoothadhoc/MainActivity.java
@@ -1,20 +1,33 @@
 package com.pratik.bluetoothadhoc;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_ENABLE_BT = 1 ;
     public static String PACKAGE_NAME = "com.bluetoothadhoc";
+    private BluetoothAdapter btAdapter;
     Map<String, String> masterProp ;
 
     // Used to load the 'native-lib' library on application startup.
@@ -32,6 +45,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(receiver);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -44,6 +64,11 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+        readCPUinfo();
+        enableBluetooth();
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
     }
 
     private void myDeviceProp() {
@@ -53,20 +78,27 @@ public class MainActivity extends AppCompatActivity {
         masterProp.put("VERSION.SDK_INT", String.valueOf(Build.VERSION.SDK_INT));
         masterProp.put("BOARD", Build.BOARD);
         masterProp.put("DEVICE", Build.DEVICE);
-
-
-       Log.i("asdf",readCPUinfo());
+        masterProp.put("CPU_MAX_FREQ",getMaxFreq());
 
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
+    private String getMaxFreq() {
+
+        String cpuMaxFreq = "0";
+        RandomAccessFile reader ;
+        try {
+            reader = new RandomAccessFile("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq", "r");
+            cpuMaxFreq = reader.readLine();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cpuMaxFreq;
+    }
+
     public native String stringFromJNI();
 
-
-    private String readCPUinfo()
+    private void readCPUinfo()
     {
         ProcessBuilder cmd;
         String result="";
@@ -86,6 +118,59 @@ public class MainActivity extends AppCompatActivity {
         } catch(IOException ex){
             ex.printStackTrace();
         }
-        return result;
+
+        Log.i("asdf",result);
+        //return result;
     }
+
+
+    public void enableBluetooth(){
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(!btAdapter.isEnabled()){
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_OK){
+            Toast.makeText(this, "bt enabled", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "please enable bt by granting permission", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private Map<String,String> getPairedDevices(){
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        Map<String,String> btPairedList = new HashMap<>();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                btPairedList.put(deviceName,deviceHardwareAddress);
+            }
+        }
+        return btPairedList;
+
+    }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                assert device != null;
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
 }
